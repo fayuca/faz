da65660961001b41694542849fb438c9bcc5a2a0
ariel ledesma <fayuca@gmail.com>
2026-05-15 01:51:17 -0300

    initial spring boot setup + hsqldb + ping endpoint

---
+++ src/main/java/com/example/faz/FazApplication.java
@@ -0,0 +1,13 @@
+package com.example.faz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/faz/FazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.faz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
